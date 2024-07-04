7738cd284e05aa2b24ff7daa91d96b5766378d3b
julianabritocordeiro <167339581+julianabritocordeiro@users.noreply.github.com>
2024-07-04 14:23:05 -0300

    else

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ let chute = prompt('Escolha um número inteiro entre 1 e 30');
 
 if (numerosecreto == chute) {
     console.log ('Isso ai! Você descobriu o número secreto (18)');
+} else {
+    alert('Você errou :(')
 }
