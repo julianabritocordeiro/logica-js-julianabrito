69319c417e24022fd03cbecdc8d248afa71e77e0
julianabritocordeiro <167339581+julianabritocordeiro@users.noreply.github.com>
2024-07-04 13:41:49 -0300

    tentando arrumar o if

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ alert('Seja bem-vindo ao Jogo do Número Secreto!!!');
 let numerosecreto = 18;
 let chute = prompt('Escolha um número inteiro entre 1 e 30');
 
-if(numerosecreto == chute) {
-console.log ('Isso ai! Você descobriu o número secreto (18)');
+if (numerosecreto == chute) {
+    console.log ('Isso ai! Você descobriu o número secreto (18)');
 }
